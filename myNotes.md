15d4f3afd831d7b39d1b4c6e5a3accd3ea3f509a
Agasalim Musayev <aga77ams@gmail.com>
2026-01-21 20:46:17 +0400

    app for selling cars created

---
+++ myNotes.md
@@ -0,0 +1,12 @@
+### Modules created
+    users module
+    reports module
+
+### Used technologies
+    TypeOrm - istifadesi
+    Slqite - istifadesi
+
+
+
+
+
---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
+@Module({
+  imports: [ReportsModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [User, Report],
+    synchronize: true, // true olanda migration-u avtomatik edilir, migration-a ehtiyac olmur. 
+                      // Amma production-da false olmalidir ki db-de avtoatik migration getmesin
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
