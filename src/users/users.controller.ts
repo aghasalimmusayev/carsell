075b9eca91cf2baeb9d754499892d6f3b8dcaaf5
Agasalim Musayev <aga77ams@gmail.com>
2026-01-23 21:08:46 +0400

    UserUpdateDto created UserEntity - Some hooks are used for logging request.http - findOne, findAll, delete and update requests are created Controller - findUser, findAllUsers, removeUser and updateUser are created Service - create, findOne, find, update and remove functions and business logings are created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Get, Controller, Param, Post, Query, Delete, Patch } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private userService: UsersService) { }
     @Post('signup')
     createUser(@Body() body: CreateUserDto) {
-        console.log(body)
+        return this.userService.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) {
+    }
+
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password })
+        return this.repo.save(user)
+        // return this.repo.save({ email, password }) //! bu sekilde de yazmaq olar, amma bu zaman DTO type-lari yoxlamiyacaq
+    }
+    findOne(id: number) {
+        return this.repo.findOneBy({ id })
+    }
+    async findOrFail(id: number): Promise<User> {
+        const user = await this.repo.findOneBy({ id })
+        if (!user) throw new NotFoundException('User not found')
+        return user
+    }
+    find(email: string) {
+        return this.repo.find({ where: { email } })
+    }
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOrFail(id)
+        Object.assign(user, attrs)
+        return this.repo.save(user)
+    }
+    async remove(id: number) {
+        const user = await this.findOne(id)
+        if (!user) throw new NotFoundException('User not found')
+        return this.repo.remove(user)
+    }
+}
+
