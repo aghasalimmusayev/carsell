e72b86c58e17c7ee2a5b0b3c2e99dc380ea5aba2
Agasalim Musayev <aga77ams@gmail.com>
2026-02-20 21:11:13 +0400

    Relation added & Report.create

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { createReportDto } from './dtos/createReportDto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { reportDto } from './dtos/reportDto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportService: ReportsService) { }
+    
+    @Post()
+    @UseGuards(AuthGuard)
+    @Serialize(reportDto)
+    createReport(@Body() body: createReportDto, @CurrentUser() user: User) {
+        return this.reportService.create(body, user)
+    }
+
+    @Get()
+    @UseGuards(AuthGuard)
+    findReports(@CurrentUser() user: User) {
+        return this.reportService.getReports(user)
+    }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { createReportDto } from './dtos/createReportDto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    constructor(@InjectRepository(Report) private repo: Repository<Report>) { }
+
+    async create(data: createReportDto, user: User) {
+        const report = this.repo.create(data)
+        report.user = user
+        return await this.repo.save(report)
+    }
+
+    async getReports(user: User) {
+        const reports = await this.repo.find({ where: { user: { id: user.id } } })
+        if (reports.length === 0) throw new NotFoundException('No reports exist')
+        return reports
+    }
+}
